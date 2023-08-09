93ddac5c811bc42c40dc93b5f47c03c5251d7992
Bill Bennett <billbenntt@gapp.nthu.edu.tw>
2023-08-09 16:05:43 +0800

    rework on styles and switching classes to BEM

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,82 +2,83 @@ import SearchBox from '../components/SearchBox.jsx'
 import MoonIcon from '/src/assets/icon-moon.svg'
 import SunIcon from '/src/assets/icon-sun.svg'
 import userImage from '/src/assets/profile-img.png'
-import userLocation from '/src/assets/icon-location.svg'
-import userLink from '/src/assets/icon-website.svg'
-import userTwitter from '/src/assets/icon-twitter.svg'
-import userCompany from '/src/assets/icon-company.svg'
+import CiLocation from '../components/icons/CiLocation.jsx'
+import CiCompany from '../components/icons/CiCompany.jsx'
+import CiTwitter from '../components/icons/CiTwitter.jsx'
+import CiWebsite from '../components/icons/CiWebsite.jsx'
 
 function Search () {
   return (
     <div className="search">
+
+      {/*SEARCH HEADER TOGGLE */}
+
       <div className="search-header">
-        <h3 className="header-title">devFinder</h3>
-        <div className="header-toggle">
+        <h3 className="search-header__title">devFinder</h3>
+        <div className="search-header__toggle">
           <h3>Dark</h3>
-          <img src={MoonIcon} alt="" className="toggle-icon"/>
+          <img src={MoonIcon} alt="" className="search-header__toggle-icon"/>
         </div>
       </div>
 
+
+      {/*SEARCH BOX */}
+
       <SearchBox/>
 
+      {/*SEARCH RESULTS */}
       <div className="search-result">
-        <div className="search-result-center">
-          <div className="user-profile-header">
+        <div className="search-result__center">
+
+          {/* SEARCH HEADER  */}
+          <div className="search-result__header">
             <div className="">
               <img src={userImage} alt="" className="user-profile-img"/>
             </div>
-            <div>
-              <p>The Octocat</p>
-              <p>@octocat</p>
+            <div className="search-result__header-info">
+              <h3>The Octocat</h3>
+              <h4>@octocat</h4>
               <p>Joined 25 Jan 2011</p>
             </div>
           </div>
 
-          <p className="user-profile-bio">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis
-            eros.
-          </p>
+          {/* SEARCH BIO */}
+          <div className="search-result__bio">
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis
+              eros.
+            </p>
+          </div>
 
-          <div className="user-profile-stats">
-            <div className="user-stat">
-              <p>Repos</p>
+          {/* STATS */}
+          <div className="search-result__stats">
+            <div className="search-result__stats-item">
+              <h4>Repos</h4>
               <p>8</p>
             </div>
-            <div className="user-stat">
-              <p>Followers</p>
+            <div className="search-result__stats-item">
+              <h4>Followers</h4>
               <p>3938</p>
             </div>
-            <div className="user-stat">
-              <p>Following</p>
+            <div className="search-result__stats-item">
+              <h4>Following</h4>
               <p>9</p>
             </div>
           </div>
-          <div className="user-profile-links">
-            <div>
-              <span>
-                <img src={userLocation} alt="" className=""/>
-              </span>
-              <a href="">San Francisco</a>
-            </div>
 
-            <div>
-              <span>
-                <img src={userLink} alt="" className=""/>
-              </span>
-              <a href="">https://github.blog</a>
+          {/*LINKS */}
+          <div className="search-result__links">
+            <div className="search-result__links-item">
+              <CiLocation/>
             </div>
-
-            <div>
-              <span>
-                <img src={userTwitter} alt="" className=""/>
-              </span>
-              <a href="">Not available</a>
+            <div className="search-result__links-item">
+              <CiCompany/>
             </div>
-            <div>
-              <span>
-                <img src={userCompany} alt="" className=""/>
-              </span>
-              <a href="">@github</a>
+            <div className="search-result__links-item">
+              <CiTwitter/>
+            </div>
+            <div className="search-result__links-item">
+              <CiWebsite/>
             </div>
           </div>
         </div>
